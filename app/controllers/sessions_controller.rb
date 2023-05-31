5f488047d60cf24f1d79a96b1a99573f0848d7e0
Christina DiMaggio <christina.dimaggio@kellogg.northwestern.edu>
2023-05-31 17:12:16 +0000

    Commit before trying something stupid

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,26 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by({"email" => params["email"]})
+    if @user != nil
+      if BCrypt::Password.new(@user["password"]) == params["password"]
+        session["user_id"] =@user["id"]
+        redirect_to "/places"
+        flash["notice"] = "Welcome, #{@user["username"]}!"
+      else    
+        flash["notice"] = "Nope."
+        redirect_to "/sessions/new" 
+      end
+    else
+      flash["notice"] = "Nope."
+      redirect_to "/sessions/new"
+    end
   end
 
   def destroy
+    session["user_id"] = nil # we have to end the cookies
+    flash["notice"] = "Goodbye."
+    redirect_to "/sessions/new"
   end
 end
   
